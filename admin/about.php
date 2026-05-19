733a3e2dc38c17c32d045206d55ca7b85a4cc855
Cooltey Feng <cfeng@wikimedia.org>
2026-05-19 11:30:37 -0700

    Update about page

--- admin/about.php
+++ admin/about.php
@@ -2,7 +2,7 @@
 /**
  * Model: C.P.Sub 公告系統
  * Author: Cooltey Feng
- * Lastest Update: 2014/6/9
+ * Lastest Update: 2025/05/19
  */
 	
 	// get json information
@@ -12,21 +12,9 @@
 	<div class="jumbotron">
 	  <div class="container">
 		<h1>感謝您的使用</h1>
-		<?php			
-			echo "<hr></hr>";
-			echo "<p>". $aboutData->msg ."</p>";			
-			echo "<hr></hr>";
-			echo "<p class=\"small_font\">最新版本：". $aboutData->latest_version;
-			if($aboutData->latest_version != $config_current_version){
-				echo "<a href=\"".$aboutData->project_link."\" target=\"_blank\" class=\"label label-warning margin_box\">前往下載最新版本</a>";
-			}
-			echo "<p class=\"small_font\">更新日期：". $aboutData->latest_update ."</p>";
-			echo "<p class=\"small_font\">作者：". $aboutData->author ."</p>";
-			$count = 0;
-			foreach($aboutData->links AS $linkKey => $linkVal){
-				echo "<a class=\"btn btn-".$label_color[$count]." btn-lg margin_box small_font\" href=\"". $linkVal ."\" target=\"_blank\">". $linkKey ."</a>";
-				$count++;
-			}
-		?>
+		<hr></hr>
+		<p>
+		歡迎前往 GitHub 頁面查看版本資訊：<a href="https://github.com/cooltey/C.P.Sub" target="_blank">https://github.com/cooltey/C.P.Sub</a>
+		</p>
 	  </div>
 	</div>
